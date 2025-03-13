386a4a7759c73c5132a5e4800f7c2df5a9bcd593
Jinhui Lyu <jh_lyu@outlook.com>
2025-03-13 16:25:09 +0800

    add should_panic test; modify test runner to handle panic scenarios

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -11,28 +11,21 @@ use blog_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
-    loop {}
+    loop{}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("should_fail... ");
     assert_eq!(0, 1);
 }
+
